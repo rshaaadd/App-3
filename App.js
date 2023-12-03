be5eee24f3b6080f798ed618e103a7fddc57fd50
rshaaadd <roshad165@gmail.com>
2023-12-03 15:58:29 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,7 @@
+import AppNavigation from './src/navigation';
+// import Categories from './src/components/categories';
+
+export default function App() {
+  return <AppNavigation />;
+  // return <Categories />;
+}
